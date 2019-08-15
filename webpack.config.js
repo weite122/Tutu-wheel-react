a44c0792786ad6ac22fb459675ab2ad8e0295c7d
weite122 <949193531@qq.com>
2019-08-15 22:38:56 +0800

    [A]webpack 配置完成

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: {
+    index: './lib/index.tsx'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/lib'),
+    library: 'Tutu-wheel-react',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Tutu-wheel-react",
+      template: 'index.html'
+    })
+  ]
+}
