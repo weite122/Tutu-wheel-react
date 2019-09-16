f87eb630d24b300854833395149960ed34d9043d
weite122 <949193531@qq.com>
2019-09-16 10:50:25 +0800

    [A]yarn/npm publish

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,16 +1,1 @@
-import Icon from './icon/icon';
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const fn: React.MouseEventHandler = (e) => {
-  console.log(e.target);
-};
-
-ReactDOM.render(<div>
-  <Icon name="wechat"
-        className="xxx"
-        onClick={fn}
-        onMouseEnter={() => {console.log('enter');}}
-        onMouseLeave={() => {console.log('leave');}}/>
-
-</div>, document.querySelector('#root'));
+export {default as Icon}from './icon/icon';
