fd838af3fcc4506b4a34236e9c0e236ed101cbd7
weite122 <949193531@qq.com>
2019-09-30 17:14:02 +0800

    [A]添加onSubmit,onChange事件

--- lib/form/form.example.tsx
+++ lib/form/form.example.tsx
@@ -1,25 +1,34 @@
 import * as React from 'react';
-import Form from './form';
+import Form, {FormValue} from './form';
 import {Fragment, useState} from 'react';
 
 const FormExample: React.FunctionComponent = () => {
-  const [formData] = useState({
-    username: '',
+  const [formData, setFormData] = useState<FormValue>({
+    username: 'xxx',
     password: ''
   });
   const [fields] = useState([
     {name: 'username', label: '用户名', input: {type: 'text'}},
     {name: 'password', label: '密码', input: {type: 'password'}}
   ]);
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    console.log(formData);
+  };
+
   return (
-    <Form value={formData} fields={fields}
-          buttons={
-            <Fragment>
-              <button type="submit">提交</button>
-              <button>返回</button>
-            </Fragment>
-          }
-    />
+    <div>
+      {JSON.stringify(formData)}
+      <Form value={formData} fields={fields}
+            buttons={
+              <Fragment>
+                <button type="submit">提交</button>
+                <button>返回</button>
+              </Fragment>
+            }
+            onChange={(newValue) => setFormData(newValue)}
+            onSubmit={onSubmit}
+      />
+    </div>
   );
 };
 
