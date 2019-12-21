4ff68d92dd6c4ee5b3cecd97c70dc85e6b592354
weite122 <949193531@qq.com>
2019-12-22 00:23:17 +0800

    [A]显示当前城市

--- lib/citySelect/citySelect.tsx
+++ lib/citySelect/citySelect.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './citySelect.scss';
 import ReactDOM from 'react-dom';
 
@@ -23,7 +23,40 @@ const Dialog: React.FC<{ onClose: () => void }> = (props) => {
   return ReactDOM.createPortal((
     <div className="tutu-citySelect-dialog"
          onClick={props.onClose}
-    >弹出内容</div>), document.body);
+    >
+      <header>
+        <span className="icon">&lt;</span>
+        <span>选择城市</span>
+      </header>
+      <CurrentLocation/>
+      <h2>全部城市</h2>
+      <div className="cityIndex">ABCD...</div>
+      <div className="cityList">所有城市</div>
+    </div>
+  ), document.body);
+}
+
+const CurrentLocation: React.FC = () => {
+  const [city, setCity] = useState<string>('加载中...')
+  useEffect(() => {
+    const xhr = new XMLHttpRequest()
+    xhr.open('get', 'http://ip-api.com/json/?lang=zh-CN')
+    xhr.onload =() => {
+      const string = xhr.responseText
+      const obj =JSON.parse(string)
+      const c = obj.city
+      setCity(c)
+    }
+    xhr.onerror = () => {
+      setCity('未知')
+    }
+    xhr.send()
+  }, [])
+  return (
+    <div className="currentCity">
+      当前城市: {city}
+    </div>
+  )
 }
 
 export default CitySelect;
